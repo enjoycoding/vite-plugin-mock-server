6b2f44f35e5c7c6fdda824a02d908c4ed3d28e80
mumashanren <mumashanren@gmail.com>
2021-06-25 12:16:52 +0800

    build as commonjs module

--- publish.bat
+++ publish.bat
@@ -1,1 +1,5 @@
-npm publish --registry=https://registry.npmjs.org/
+@echo off
+rem publish vite-plugin-mock-server to https://registry.npmjs.org/
+
+set NPM_ARGS="--registry=https://registry.npmjs.org/"
+npm login %NPM_ARGS% && npm publish %NPM_ARGS%
