2ce2f6a35ad51dc25617b2db14c81a9804b2b364
reinerBa <4329883+reinerBa@users.noreply.github.com>
2024-01-04 17:20:06 +0100

    Add Runtime-test

--- example2/vite.config.ts
+++ example2/vite.config.ts
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, Plugin, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 //import mockServer from 'vite-plugin-mock-server'
 import mockServer from '../dist/index.js'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(), 
-    mockServer()
-  ],
-})
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+
+  if(mode === 'test')
+    return {
+      plugins: [
+        vue(), 
+        mockServer(),
+        VitePluginRestart()
+    ]}
+  else 
+    return {
+      plugins: [
+        vue(), 
+        mockServer()
+    ]}
+}
+
+
+
+function VitePluginRestart(options = {delay: 5000}): Plugin {
+  const {delay} = options
+
+  return {
+    name: `vite-plugin-stop`,
+    apply: 'serve',
+    configureServer(server) {
+      setTimeout(()=> {
+        server.close()
+        setTimeout(process.exit, 1e3)
+      }
+        , delay)
+    },
+  }
+}
